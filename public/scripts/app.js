b8973cfcca78e0492c675816d7647f5c206ff12a
jaymeramsay <jayme.ramsay@gmail.com>
2018-12-12 12:12:50 -0600

    sets up babel compilation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,12 @@
-console.log("App is running!")
+'use strict';
 
-const template = <p>This is JSX from app.js</p>
-const appRoot = document.getElementById('app');
+console.log('App is running!');
+
+var template = React.createElement(
+  'p',
+  null,
+  'This is jsx from my app.js file'
+);
+var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,6 @@
+console.log('App is running!')
+
+const template = <p>This is jsx from my app.js file</p>
+const appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
