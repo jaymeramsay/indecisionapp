dd96517be7a6efd1f139605b8a4526d149fa8f9b
jaymeramsay <jayme.ramsay@gmail.com>
2018-12-13 12:53:16 -0600

    adds conditional statements

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,101 @@
 
 console.log('App is running!');
 
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Sometimes you need something else to decide for you',
+    options: ['one', 'two']
+};
 var template = React.createElement(
-  'p',
-  null,
-  'This is jsx from my app.js file'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'h2',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options:' : 'There are no available options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
 );
+
+var user = {
+    name: 'Jeannie',
+    subtitle: 'The story of a complex person',
+    age: 34,
+    location: 'Atlanta',
+    bands: ['PJ Harvey', 'Andreya Trinana', 'The Gun Club']
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    };
+}
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.subtitle && React.createElement(
+        'h2',
+        null,
+        user.subtitle
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location),
+    React.createElement(
+        'p',
+        null,
+        'Favorite Music'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        user.bands.map(function (band) {
+            return React.createElement(
+                'li',
+                null,
+                band
+            );
+        })
+    )
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,52 @@
 console.log('App is running!')
 
-const template = <p>This is jsx from my app.js file</p>
+let app = {
+    title: 'Indecision App',
+    subtitle: 'Sometimes you need something else to decide for you',
+    options: ['one', 'two']
+}
+const template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <h2>{app.subtitle}</h2>}
+        <p>{app.options.length > 0 ? 'Here are your options:' : 'There are no available options'}</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+);
+
+let user = {
+    name: 'Jeannie',
+    subtitle: 'The story of a complex person',
+    age: 34,
+    location: 'Atlanta',
+    bands: [
+        'PJ Harvey',
+        'Andreya Trinana',
+        'The Gun Club'
+    ]
+}
+
+function getLocation (location) {
+    if(location) {
+        return <p>Location: {location}</p>;
+    };   
+}
+const templateTwo = (
+    <div>
+        <h1>{ user.name ? user.name : 'Anonymous'}</h1>
+        {user.subtitle && <h2>{user.subtitle}</h2>}
+        { (user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+        <p>Favorite Music</p>
+        <ol>
+            {user.bands.map(band => <li>{band}</li>)}
+        </ol>
+    </div>
+)
+
 const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
