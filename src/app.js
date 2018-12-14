78d4a52ce0dd35eb1c6303dcfb281aa8e5600ed1
jaymeramsay <jayme.ramsay@gmail.com>
2018-12-14 14:38:47 -0600

    adds random feature to indecision app

--- src/app.js
+++ src/app.js
@@ -3,12 +3,11 @@ console.log('App is running!')
 const app = {
     title: 'Indecision App',
     subtitle: 'Sometimes you need something else to decide for you',
-    options: ['one', 'two']
+    options: []
 }
 
 const onFormSubmit = (e) => {
     e.preventDefault();
-    console.log(e)
     const option = e.target.elements.option.value;
 
     if (option) {
@@ -23,6 +22,12 @@ const onRemoveAllOptions = () => {
     renderIndecisionApp();
 }
 
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum]
+    alert(option);
+}
+
 const appRoot = document.getElementById('app');
 
 const renderIndecisionApp = () => {
@@ -32,10 +37,11 @@ const renderIndecisionApp = () => {
             {app.subtitle && <h2>{app.subtitle}</h2>}
             <p>{app.options.length > 0 ? 'Here are your options:' : 'There are no available options'}</p>
             <button onClick={onRemoveAllOptions}>Remove All Options</button>
-            <p>{app.options.length}</p>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What Should I do?</button>
             <ol>
-                <li>Item One</li>
-                <li>Item Two</li>
+               {
+                   app.options.map((option) => <li key={option}>{option}</li>)
+               }
             </ol>
             <form onSubmit={onFormSubmit}>
             <input type="text" name="option"/> 
