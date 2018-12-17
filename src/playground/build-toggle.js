b469cacb97d5e8d144b1be26af654078d5ca1d80
jaymeramsay <jayme.ramsay@gmail.com>
2018-12-17 14:55:38 -0600

    refactors using react components and state

--- src/playground/build-toggle.js
+++ src/playground/build-toggle.js
@@ -1,21 +1,50 @@
-let visibility = false;
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visibility: false
+        }
+    }
 
-const toggleVisibility = () => {
-    visibility = !visibility
-    renderToggle();
+    handleToggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            }
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h1>Invisible Toggle</h1>
+                <button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'Hide Details' : 'Show Details'}</button>
+                {this.state.visibility && <p>Oh, you know it feels good, feels good, feels good.</p>}
+            </div>
+        )
+    }
 }
 
-const appRoot = document.getElementById('app');
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'))
 
-const renderToggle = () => {
-    const template = (
-        <div>
-            <h1>Invisible Toggle</h1>
-            <button onClick={toggleVisibility}>{visibility ? 'Hide Details' : 'Show Details'}</button>
-            {visibility && <p>You show the lights that stop me turn to stone; you shine me when Im alone</p>}
-        </div>
-    );
-    ReactDOM.render(template, appRoot);
-};
+// let visibility = false;
 
-renderToggle();
+// const toggleVisibility = () => {
+//     visibility = !visibility
+//     renderToggle();
+// }
+
+// const appRoot = document.getElementById('app');
+
+// const renderToggle = () => {
+//     const template = (
+//         <div>
+//             <h1>Invisible Toggle</h1>
+//             <button onClick={toggleVisibility}>{visibility ? 'Hide Details' : 'Show Details'}</button>
+//             {visibility && <p>You show the lights that stop me turn to stone; you shine me when Im alone</p>}
+//         </div>
+//     );
+//     ReactDOM.render(template, appRoot);
+// };
+
+// renderToggle();
