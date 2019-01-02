f61ae159d3d8715461db7383f5e429778a00f4a5
jaymeramsay <jayme.ramsay@gmail.com>
2019-01-02 12:23:21 -0600

    imports react and react-dom

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import validator from 'validator';
 
-console.log(validator.isEmail('test'));
+const template = React.createElement('p', {}, 'testing');
+
+ReactDOM.render(template, document.getElementById('app'));
 
 
